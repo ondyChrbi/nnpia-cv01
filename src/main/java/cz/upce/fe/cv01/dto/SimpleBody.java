437c8be8e694c7b52da3fee3b4817811db377648
ondyChrbi <ondrej.chrbolka@icloud.com>
2023-02-13 19:25:45 +0100

    2.	✏️ Při funkčnosti řešení zkuste tuto třídu přepsat s využitím Loombooku (tedy tak aby obsahovala pouze anotace a atributy, nic jiného).

--- src/main/java/cz/upce/fe/cv01/dto/SimpleBody.java
+++ src/main/java/cz/upce/fe/cv01/dto/SimpleBody.java
@@ -1,27 +1,13 @@
 package cz.upce.fe.cv01.dto;
 
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+// Or @Data annotation for all above
 public class SimpleBody {
     private String message;
-
-    public SimpleBody() {
-    }
-
-    public SimpleBody(String message) {
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    @Override
-    public String toString() {
-        return "SimpleBody{" +
-                "message='" + message + '\'' +
-                '}';
-    }
 }
