88e2fa7614612053807f6835ef180356dc596947
ondyChrbi <ondrej.chrbolka@icloud.com>
2023-02-13 19:07:16 +0100

    5.	✏️ Vytvořte inicializační commit s kostrou projektu. 6.	✏️ Vytvořte si účet na GitHub, GitLab nebo obdobné službě. 7.	✏️ Pushněte commity na vzdálený repositář.
    9.	V adresáři pro zdrojové kódy vytvořte novou Java/Kotlin třídu s názvem HelloController.java/HelloController.kt.
    10.	Třídu opatřete anotacemi:
    a.	@RestController
    b.	@RequestMapping("/api/v1")
    c.	❓Do komentáře nad třídou napište rozdíl mezi anotací @RestController a @Controller.
    11.	Do třídy vložte novou metodu s následující signaturou:

---
+++ src/main/java/cz/upce/fe/cv01/controller/HelloController.java
@@ -0,0 +1,15 @@
+package cz.upce.fe.cv01.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class HelloController {
+    @GetMapping("")
+    public String helloWorld() {
+        return "Hello world from Spring Boot application.";
+    }
+
+}
